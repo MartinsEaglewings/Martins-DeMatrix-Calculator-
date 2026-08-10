4052fdcad0eda0913c57dcbe4a75d92151e1df7f
MartinsEaglewings <albertgmartins17@gmail.com>
2026-08-10 19:47:51 +0100

    Add WebView to MainActivity for local HTML loading
    Implement a WebView in MainActivity to load local HTML.

---
+++ app/src/main/app/src/main/java/com/martins/dematrix/MainActivity.java
@@ -0,0 +1,62 @@
+package com.martins.dematrix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onPermissionRequest(
+                    final PermissionRequest request) {
+
+                runOnUiThread(() ->
+                    request.grant(
+                        request.getResources()
+                    )
+                );
+            }
+        });
+
+        webView.loadUrl(
+            "file:///android_asset/index.html"
+        );
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    }
